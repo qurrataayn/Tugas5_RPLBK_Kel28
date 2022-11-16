404d0750e32a72a6160811164c930ec4c4efe1f4
qurrataayn <63760694+qurrataayn@users.noreply.github.com>
2022-11-16 07:36:01 +0700

    Form create

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -29,7 +29,34 @@ const Create = () => {
           <button className="btn btn-primary">Show Data</button>
         </Link>
       </div>
-      
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </form>
     </>
   );
 };
